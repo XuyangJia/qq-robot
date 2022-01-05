88599e59ec3ca01b3e1181aff4290b0395220ff8
XuyangJia <jiaxuyang@outlook.com>
2022-01-06 00:10:40 +0800

    Update default.js
    修改文件读取路径

--- api/src/config/default.js
+++ api/src/config/default.js
@@ -2,6 +2,6 @@ import { readFileSync } from 'fs'
 import { dirname, resolve } from 'path'
 import yaml from 'js-yaml'
 
-const configDir = dirname(decodeURI(import.meta.url).replace(/^file:\/\/\//, ''))
-const env = yaml.load(readFileSync(resolve(configDir, '../../config.yml'), 'utf8'))
+// const configDir = dirname(decodeURI(import.meta.url).replace(/^file:\/\/\//, ''))
+const env = yaml.load(readFileSync('./config.yml', 'utf8'))
 export { env }

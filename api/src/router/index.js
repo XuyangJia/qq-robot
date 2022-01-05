e627233782929772b81f8f212a01917663df1c0e
XuyangJia <jiaxuyang@outlook.com>
2022-01-06 00:12:17 +0800

    Update index.js
    修改路由读取路径

--- api/src/router/index.js
+++ api/src/router/index.js
@@ -3,7 +3,8 @@ import { dirname } from 'path'
 import Router from '@koa/router'
 
 const router = new Router()
-const routerDir = dirname(decodeURI(import.meta.url).replace(/^file:\/\/\//, ''))
+// const routerDir = dirname(decodeURI(import.meta.url).replace(/^file:\/\/\//, ''))
+const routerDir = '.'
 readdirSync(routerDir).forEach(file => {
   if (file !== 'index.js') {
     import(`./${file}`).then(r => router.use(r.router.routes()))

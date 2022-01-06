844534bb5b84688152f82577f3c58dbc4c941ba1
“JiaXuyang” <jiaxuyang@outlook.com>
2022-01-06 14:24:56 +0800

    修改启动API的命令

--- api/src/config/default.js
+++ api/src/config/default.js
@@ -1,7 +1,5 @@
 import { readFileSync } from 'fs'
-import { dirname, resolve } from 'path'
 import yaml from 'js-yaml'
 
-// const configDir = dirname(decodeURI(import.meta.url).replace(/^file:\/\/\//, ''))
 const env = yaml.load(readFileSync('./config.yml', 'utf8'))
 export { env }
--- api/src/router/index.js
+++ api/src/router/index.js
@@ -1,9 +1,7 @@
 import { readdirSync } from 'fs'
-import { dirname } from 'path'
 import Router from '@koa/router'
 
 const router = new Router()
-// const routerDir = dirname(decodeURI(import.meta.url).replace(/^file:\/\/\//, ''))
 const routerDir = './src/router'
 readdirSync(routerDir).forEach(file => {
   if (file !== 'index.js') {
